68514b1ae962513bebdb272ff812532ad1aee20e
Paul Esch-Laurent <p@uly.io>
2024-05-31 00:04:57 -0500

    fix: underline current nav link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import type React from "react";
 
 interface LayoutProps {
@@ -7,6 +8,8 @@ interface LayoutProps {
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const pathname = usePathname()
+
   return (
     <div className="flex flex-col h-screen">
       <Head>
@@ -22,10 +25,10 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
 
       <div className="border-b">
         <div className="flex items-center gap-4 container mx-auto py-2">
-          <Link className="text-blue-500" href="/documents">
+          <Link className={`text-blue-500 ${pathname === "/documents" ? 'underline' : ''}`} href="/documents">
             Documents
           </Link>
-          <Link className="text-blue-500" href="/people">
+          <Link className={`text-blue-500 ${pathname === "/people" ? 'underline' : ''}`} href="/people">
             People
           </Link>
         </div>
